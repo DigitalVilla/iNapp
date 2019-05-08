9493791d4ed8ba43f5b7c81004c647cb157dfca2
Omar Villanueva <villa@Omars-MacBook-Pro.local>
2019-05-08 17:25:34 -0600

    Setup filnilized

--- App.js
+++ App.js
@@ -4,29 +4,18 @@ import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={viewStyles}>
-        <TouchableHighlight
-          style={[buttonStyles.core, buttonStyles.hairlineBorder,
-          buttonStyles.spacer]}
-          underlayColor="#efefef"
-          activeOpacity={0.8}
-          onPress={() => { }}
-        >
-          <Text>Default Hairline</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={[buttonStyles.core, buttonStyles.spacer]}>
-          <Text>Default Normal</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={[buttonStyles.core, buttonStyles.hairlineBorder, buttonStyles.spacer]}>
-          <Text>Default Hairline</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={[buttonStyles.core, buttonStyles.primary, buttonStyles.spacer]}>
-          <Text>Primary Normal</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={[buttonStyles.core, buttonStyles.primary, buttonStyles.hairlineBorder]}>
-          <Text>Primary Hairline</Text>
-        </TouchableHighlight>
+      //https://facebook.github.io/react-native/docs/flexbox
+      // flexDirection: row (default), column
+      //align on primary axis
+      // justifyContent: flex-start, center, flex-end, space-around, space-between and space-evenly.
+      //align on secondary axis
+      // alignItems: flex-start, center, flex-end, and stretch.
+      //note For stretch to have an effect, children must not have a fixed dimension along the secondary axis. In the following example, setting alignItems: stretch does nothing until the width: 50 is removed from the children.
+
+      <View style={{ flex: 1, justifyContent: 'space-between', alignItems:'flex-end' }}>
+        <View style={{ width: 100, height: 100, backgroundColor: 'powderblue' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'steelblue' }} />
       </View>
     );
   }
@@ -46,7 +35,7 @@ const buttonStyles = StyleSheet.create({
     borderWidth: 1,
     backgroundColor: '#eee',
     borderRadius: 3,
-    padding: 3,
+    padding: 10,
     paddingLeft: 5,
     paddingRight: 5
   },
