698eab131f6229a210be8669c32e451daf841368
Omar Villanueva <villa@Omars-MacBook-Pro.local>
2019-05-08 14:58:09 -0600

    Setup filnilized

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> New expo Project Alive!</Text>
+      <View style={viewStyles}>
+        <TouchableHighlight
+          style={[buttonStyles.core, buttonStyles.hairlineBorder,
+          buttonStyles.spacer]}
+          underlayColor="#efefef"
+          activeOpacity={0.8}
+          onPress={() => { }}
+        >
+          <Text>Default Hairline</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={[buttonStyles.core, buttonStyles.spacer]}>
+          <Text>Default Normal</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={[buttonStyles.core, buttonStyles.hairlineBorder, buttonStyles.spacer]}>
+          <Text>Default Hairline</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={[buttonStyles.core, buttonStyles.primary, buttonStyles.spacer]}>
+          <Text>Primary Normal</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={[buttonStyles.core, buttonStyles.primary, buttonStyles.hairlineBorder]}>
+          <Text>Primary Hairline</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const viewStyles = {
+  alignItems: 'center',
+  backgroundColor: '#ddd',
+  flex: 1,
+  justifyContent: 'center',
+};
+
+const buttonStyles = StyleSheet.create({
+  core: {
+    borderStyle: 'solid',
+    borderColor: '#d5d5d5',
+    borderWidth: 1,
+    backgroundColor: '#eee',
+    borderRadius: 3,
+    padding: 3,
+    paddingLeft: 5,
+    paddingRight: 5
   },
+  primary: {
+    backgroundColor: '#60b044',
+    borderColor: '#355f27'
+  },
+  hairlineBorder: {
+    borderWidth: StyleSheet.hairlineWidth
+  },
+  spacer: {
+    marginBottom: 10
+  }
 });
